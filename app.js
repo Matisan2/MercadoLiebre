704905931d4d4258f7c25ee7773b83e289f1869a
Matisan2 <matisan2@hotmail.com>
2021-07-01 20:22:07 -0300

    subiendo a git

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const path = require('path');
+
+app.use(express.static(path.resolve(__dirname, './public')));
+app.use(express.static(path.resolve(__dirname, './views')));
+
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Servidor corriendo");
+})
+
+app.get("/", (req,res)=>{
+  res.sendFile (path.resolve(__dirname, './views/home.html'))
+})
